788192ac27b7f6a0b70cf587eb5bc8d8c25d41f7
Jnv27 <aniketpathak27@gmail.com>
2021-05-22 16:14:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb://localhost:27017/nodejs-auth';
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database connected');
+});
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
+app.listen(3000, () =>{
+  console.log('server has started');
+})
